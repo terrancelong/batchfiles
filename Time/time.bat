0f1d782b180365ec620614d8e5f92ec9e31232de
terrancelong <terrancelong@gmail.com>
2020-11-04 12:08:41 -0600

    Delete time.bat

--- Time/time.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal ENABLEDELAYEDEXPANSION
-
-set today=!date:/=-!
-set now=!time::=-!
-set millis=!now:*.=!
-set now=!now:.%millis%=!
-
-echo hello > "Today is !today! and the time is !now!.txt"
